09f9aa5fdef57b37619ade30b3f2cf52e73bce9b
AleksiEnsioLaine <aleksilaine91@gmail.com>
2017-03-27 14:00:50 +0300

    it's working

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ const MongoClient = require('mongodb').MongoClient
 
 var db;
 
-MongoClient.connect('mongodb://root:1234@ds137550.mlab.com:37550/quotes', (err, database) => {
+MongoClient.connect('mongodb://root:1234@ds137550.mlab.com:37550/messages', (err, database) => {
   if (err) return console.log(err);
   db = database;
   app.listen(666, () => {
     console.log('I\'m running at port 666 hooray');
-  });
+  })
 });
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -23,14 +23,14 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  db.collection('quotes').find().toArray((err, result) => {
+  db.collection('messages').find().toArray((err, result) => {
     if (err) return console.log(err);
-    res.render('pages/index.ejs', {quotes: result})
+    res.render('pages/index.ejs', {messages: result})
   })
 })
 
-app.post('/quotes', (req, res) => {
-  db.collection('quotes').save(req.body, (err, result) => {
+app.post('/messages', (req, res) => {
+  db.collection('messages').save(req.body, (err, result) => {
   if (err) return console.log(err);
 
   console.log('saved to database');
